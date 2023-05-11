a5ab5869a1e2e7c27507416eba21ba3e9212c05b
Nicky <n.tabrizifar@student.fontys.nl>
2023-05-11 11:56:21 +0200

    testing a different redirect uri

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ import Profile from "./Profile";
 const CLIENT_ID = "99e8f40ff31e4773afd9025afb9d63c2";
 const CLIENT_SECRET = "a0c007ed7ada4e0aa5eabfeb02a6ffc9";
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/login";
+const REDIRECT_URL_AFTER_LOGIN = "https://nickster.vercel.app/Login";
 const SPACE_DELIMITER = "%20";
 const redirectUri = "http://localhost:3000/login";
 const scopes = ["user-read-currently-playing", "user-read-playback-state"];
